156ed28594b44ffea964414c0aa22b2fc1c40d13
juferoga <juancholoco2000.61@gmail.com>
2021-05-26 18:08:16 -0500

    routes and redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { HeaderModule } from '@shared/components/header/header.module';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
-
+// Exportamos para poder utilizarlo en toda la aplicacion.
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [ 
+    HeaderComponent
   ]
 })
 export class HeaderModule { }
